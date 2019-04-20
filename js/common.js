acd1dffdcee9191e6e4da2bcf77bfd74af30db13
Aslan <berezg86@gmail.com>
2019-04-20 23:31:23 +0300

    20.04

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    // $('.clientsBox-item a').hover(function() {
+    //     $(this).toggleClass('linkAktive');
+    // });
+
+     $('.phone-mask').mask('+7(999) 999-9999');
+
+
+}); //END READY
